ff47f3fcf041edde335b3d44eef964e1a7ab90ff
ruy <7leckler@gmail.com>
2025-08-31 17:14:17 -0300

    feat: input component

---
+++ web/src/components/TheInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <label>
+    {{ label }}
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheInput",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: {
+    "update:modelValue": (value: string) => typeof value === "string",
+  },
+});
+</script>
