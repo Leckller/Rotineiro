c947b3333bb7b9759aa79690ae2062c577106fbf
ruy <7leckler@gmail.com>
2025-09-06 18:45:21 -0300

    feat: css da tela de home completa

--- web/src/components/Home/HomeRoutineProgress.vue
+++ web/src/components/Home/HomeRoutineProgress.vue
@@ -1,33 +1,73 @@
 <template>
-
-  <h2 class="routine-card">
+  <div class="routine-progress">
     <p>
       Progresso do dia
+      <span class="percent">
+        {{ getPercent() }} %
+      </span>
     </p>
-    <span>
-      {{((routine.tasks.filter((t: TaskEntity) => t.completed).length / routine.tasks.length) * 100).toFixed(1)}} %
-    </span>
-  </h2>
-
+    <ProgressBar :color="'black'" :value="getPercent()" />
+    <p class="qtd-tasks-status">
+        {{ getCompletedTasks() }} de {{ routine.tasks.length }} tarefas concluídas
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
 import { Routine } from '@/services/routineService';
 import { TaskEntity } from '@/services/taskService';
 import { defineComponent } from 'vue';
+import ProgressBar from '../ProgressBar.vue';
 
 export default defineComponent({
+  name: "HomeRoutineProgress",
+  components: {
+    ProgressBar
+  },
   props: {
     routine: { required: true, type: Routine }
   },
-})
-
+  methods: {
+    getCompletedTasks() {
+      return this.routine.tasks.filter((t: TaskEntity) => t.completed).length
+    },
+    getPercent() {
+      if (!this.routine.tasks.length) return 0;
+      return (
+        (this.routine.tasks.filter((t: TaskEntity) => t.completed).length /
+          this.routine.tasks.length) *
+        100
+      ).toFixed(0);
+    }
+  }
+});
 </script>
 
 <style scoped>
-.routine-card {
+.qtd-tasks-status {
+  font-size: 12px;
+  color: rgb(100, 100, 100);
+}
+
+.routine-progress {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 300px;
+  gap: 8px;
+  border: solid 1px rgb(163, 163, 255);
+  border-radius: 8px;
+  padding: 16px;
+  background: linear-gradient(to right, #acd8ff5b, #c3ffd658);
+}
+
+.routine-progress p {
   display: flex;
   justify-content: space-between;
-  gap: 16px;
+}
+
+.percent {
+  font-size: large;
+  color: rgb(109, 109, 248);
 }
 </style>
--- web/src/components/Home/HomeRoutineTask.vue
+++ web/src/components/Home/HomeRoutineTask.vue
@@ -1,56 +1,53 @@
 <template>
-
   <div class="routine-task-cards">
     <article class="task-card" v-for="task in routine.tasks" :key="task.id">
-      <input :checked="task.completed" type="checkbox" @click="toggleTask(task.id)">
+      <input 
+        type="checkbox" 
+        v-model="task.completed"
+        @change="toggleTask(task)"
+      >
       <div>
-        <h4> {{ task.name }} </h4>
-        <p>
-          <small>
-            {{ task.estimate }} min
-          </small>
-        </p>
+        <h4>{{ task.name }}</h4>
+        <p><small>{{ task.estimate }} min</small></p>
       </div>
-      <button @click="startTask(task.id)">
+      <button @click="startTask(task)">
         <FontAwesomeIcon icon="play" />
       </button>
     </article>
   </div>
-
 </template>
 
 <script lang="ts">
 import { Routine } from '@/services/routineService';
-import { TaskService } from '@/services/taskService';
+import { Task, TaskService } from '@/services/taskService';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "HomeRoutineTask",
-  components: {
-    FontAwesomeIcon
+  components: { FontAwesomeIcon },
+  props: {
+    routine: { required: true, type: Object as () => Routine }
   },
   methods: {
-    async toggleTask(taskID: number) {
+    async toggleTask(task: Task) {
+      const prevState = task.completed;
       try {
-        await TaskService.toggleTask(taskID);
+        await TaskService.toggleTask(task.id);
       } catch (error) {
-        console.log(error);
+        console.error(error);
+        task.completed = prevState;
       }
     },
-    async startTask(taskID: number) {
+    async startTask(task: Task) {
       try {
-        await TaskService.startTask(taskID);
+        await TaskService.startTask(task.id);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
-    },
-  },
-  props: {
-    routine: { required: true, type: Routine }
-  },
-})
-
+    }
+  }
+});
 </script>
 
 <style scoped>
