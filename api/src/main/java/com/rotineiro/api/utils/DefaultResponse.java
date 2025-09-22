5963015cbfa7c71eebe079a3721f8f0d1c43ec4d
Gusttavo Ruy <7leckler@gmail.com>
2025-09-22 08:50:32 -0300

    feat: padronização dos erros

--- api/src/main/java/com/rotineiro/api/utils/DefaultResponse.java
+++ api/src/main/java/com/rotineiro/api/utils/DefaultResponse.java
@@ -2,10 +2,12 @@ package com.rotineiro.api.utils;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class DefaultResponse<GenericResponse> {
