92e8ab7a802741fc886c4c3e800ad9d16b1a769b
Gusttavo Ruy <7leckler@gmail.com>
2026-09-02 18:52:31 -0300

    refactor: estrutura inicial em go

--- api/src/main/java/com/rotineiro/api/repository/RoutineScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rotineiro.api.repository;
-
-import com.rotineiro.api.repository.entities.RoutineSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineScheduleRepository extends JpaRepository<RoutineSchedule, Integer> {
-}
--- api/src/main/java/com/rotineiro/api/security/exceptions/InternalServerError.java
+++
@@ -1,7 +0,0 @@
-package com.rotineiro.api.security.exceptions;
-
-public class InternalServerError extends RuntimeException {
-  public InternalServerError(String message) {
-    super(message);
-  }
-}
